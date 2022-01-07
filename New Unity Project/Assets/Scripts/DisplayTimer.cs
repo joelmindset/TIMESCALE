7de380a9b5d7506a547c280e9165a62b6d59fddb
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2022-01-06 20:30:47 -0700

    Fixes Almost There

--- New Unity Project/Assets/Scripts/DisplayTimer.cs
+++ New Unity Project/Assets/Scripts/DisplayTimer.cs
@@ -11,20 +11,34 @@ public class DisplayTimer : MonoBehaviour
     public TMP_Text daysCount;
     public TMP_Text frametimer;
     public TMP_Text gameTimer;
+    public TMP_Text modifierValue;
+    public TMP_Text gameSeconds;
 
     private float timer = 0.0f;
     private bool isTimer = false;
     public float multiplier = 1;
-    public float modifiedScale = 0;
+    public float modifiedScale;
+
     [SerializeField]
     private int days = 0;
 
     public DateTime gametime;
     public DateTime rttimer;
-    public float RT_Hours;
-    public float RT_Minutes;
-    public float RT_Seconds;
+    
+    public int RT_Hours;
+    public int RT_Minutes;
+    public int RT_Seconds;
+    
+
+    public int hours;
+    public int minutes;
+    public int seconds;
+
     public int framecounter;
+    public int G_secSaver;
+    public int G_secCounter = 0;
+    public int RT_secCounter = 0;
+    public int RT_secSaver;
 
 
     void Start()
@@ -34,27 +48,33 @@ public class DisplayTimer : MonoBehaviour
         RT_Minutes = rttimer.Minute;
         RT_Seconds = rttimer.Second;
         gametime = DateTime.Now;
-        gameTimer.text = string.Format("{0:00}:{1:00}:{2:00}", RT_Hours, RT_Minutes, RT_Seconds);
+        gameTimer.text = string.Format("{0:00}:{1:00}:{2:00}", RT_Hours * modifiedScale, RT_Minutes, RT_Seconds);
     }
     // Update is called once per frame
     void Update()
     {
+
         framecounter++;
         frametimer.text = Convert.ToString("Frames: " + framecounter);
+
         if (isTimer)
         {
             timer += Time.deltaTime * multiplier;
             DisplayTime();
         }
+
         rttimer = DateTime.Now;
-       
-        RTtextTimer.text = $"{rttimer:HH:mm:ss}";
-        
+
+
         Time.timeScale = modifiedScale;
 
+        RTtextTimer.text = $"{rttimer:HH:mm:ss}";
         daysCount.text = Convert.ToString("Days: " + days);
+        modifierValue.text = Convert.ToString("Modifier Value: " + modifiedScale);
+        gameSeconds.text = Convert.ToString("Game Secs: " + G_secCounter);
     }
 
+    
     void DisplayTime()
     {
         if(timer >= 60.0f * 60.0f * 24.0f)
@@ -62,9 +82,16 @@ public class DisplayTimer : MonoBehaviour
             timer -= 60.0f * 60.0f * 24.0f;
             days++;
         }
-        int hours = Mathf.FloorToInt(timer / (60.0f * 60.0f));
-        int minutes = Mathf.FloorToInt(timer / 60.0f - hours * 60);
-        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        
+        hours = Mathf.FloorToInt(timer / (60.0f * 60.0f));
+        minutes = Mathf.FloorToInt(timer / 60.0f - hours * 60);
+        seconds = Mathf.FloorToInt(timer - minutes * 60);
+        if (G_secSaver != seconds)
+        {
+            G_secCounter++;
+        }
+        G_secSaver = seconds;
+       
         textTimer.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
     }
     
